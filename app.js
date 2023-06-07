f78e803cd5c13c7f2db3c79bb11598f32ee0740c
scasasr <CORREO@example.com>
2023-06-07 15:22:54 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
+
+
+const app = express();
+
+app.use(cors());
+
+const PORT = 5000
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const staticRoute = path.join(__dirname, '/uploads')
+
+app.use('/uploads/',express.static(staticRoute))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads')
+    },
+    filename: (req, file, cb) =>{
+        const ext = file.originalname.split('.').pop()
+        cb(null,`${Date.now()}.${ext}`)
+    }
+})
+
+const upload = multer({storage})
+
+app.post('/upload',upload.single('file'), (req,res) => {
+    console.log('request')
+    console.log(req.file.filename)
+    res.status(200).json({nameImage:req.file.filename});
+})
+    
+
+
+app.listen(PORT,()=>{
+    console.log('Servidor iniciado en el puerto ', PORT);
+})
